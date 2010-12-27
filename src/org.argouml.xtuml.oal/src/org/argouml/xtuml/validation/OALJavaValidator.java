091c11576d5b9045eff67ca50517f90f4b378945
Linus Tolke <linus@tigris.org>
2010-12-27 15:55:26 +0000

    First experiments with xtext and oal language.
    git-svn-id: http://argouml-xtuml.tigris.org/svn/argouml-xtuml/trunk@5 7cb2c96d-fc14-4809-b907-eacae0a3cae7

---
+++ src/org.argouml.xtuml.oal/src/org/argouml/xtuml/validation/OALJavaValidator.java
@@ -0,0 +1,11 @@
+package org.argouml.xtuml.validation;
+
+public class OALJavaValidator extends AbstractOALJavaValidator {
+
+//	@Check
+//	public void checkGreetingStartsWithCapital(Greeting greeting) {
+//		if (!Character.isUpperCase(greeting.getName().charAt(0))) {
+//			warning("Name should start with a capital", MyDslPackage.GREETING__NAME);
+//		}
+//	}
+}
